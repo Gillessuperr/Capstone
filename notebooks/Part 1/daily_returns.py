5c40da52e2f81c9595200f44a638c09d8f57c654
Gilles <gillesvandenboom@hotmail.com>
2023-08-25 22:22:03 +0200

    Change in filesystem

--- notebooks/Part 1/daily_returns.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-JPM_df = pd.read_csv('JPM.csv')
-JPM_df['Daily Return'] = JPM_df['Adj Close'].pct_change(1) * 100
-print(JPM_df['Daily Return'].max())
--- notebooks/Part 1/missing_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-JPM_df = pd.read_csv('JPM.csv')
-JPM_df.isnull().sum()
-JPM_df.info()
